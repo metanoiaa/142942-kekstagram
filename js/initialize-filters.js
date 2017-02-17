0767795e731b879442caf1d7f373e026de086fe0
Nadi Ermakova <nadi@staal.io>
2017-02-17 12:05:13 +0100

    Перенесла callback в обработчик, убрала лишнюю переменную

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,16 +1,13 @@
 'use strict';
 
 window.initializeFilters = (function () {
-  var imagePreview = document.querySelector('.filter-image-preview');
 
   // Применим фильтры к изображению
-  return function (callback) {
+  return function (className, callback) {
 
     document.addEventListener('change', function (event) {
       var className = event.target.id.replace('upload-', '');
-      imagePreview.classList.add(className);
+      callback(className);
     });
-
-    callback(className);
   };
 })();
