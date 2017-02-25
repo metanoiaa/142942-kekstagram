6ce667e7e65b5735eea8bd9a561e099809efa535
Nadi Ermakova <nadi@staal.io>
2017-02-26 00:03:24 +0100

    В цикле прошлась по записям в массиве pictures и создала DOM-элемент на основе шаблона #picture-template

--- js/pictures.js
+++ js/pictures.js
@@ -8,7 +8,19 @@ var onLoad = function (event) {
   pictures = JSON.parse(event.target.response);
 
   pictures.forEach(function (picture) {
-    console.log(picture);
+    // console.log(picture);
+    var templateElement = document.querySelector('template');
+    var elementToClone = templateElement.content.querySelector('a.picture');
+    var pictureLikes = document.querySelector('.picture-likes');
+    var pictureComments = document.querySelector('.picture-comments');
+
+    var newElement = elementToClone.cloneNode(true);
+    var newPictires = document.querySelector('.pictures');
+
+    document.body.appendChild(newElement);
+    newElement.addEventListener('click', function() {
+      alert('Я');
+    });
   });
 };
 
